54630e0c89900ee8b8e2deb02a2f323de3400d80
Lee-jun-hee <wns8432@naver.com>
2016-02-14 09:56:34 +0900

    [최종본 커밋]

---
+++ Unithon/Assets/Script/BackBtn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackBtn : MonoBehaviour 
+{
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                // 할꺼 하셈
+                Application.Quit();
+            }
+        }
+    }
+
+}
