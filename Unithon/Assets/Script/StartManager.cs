1291439fdb92c6a50fa1a5cc3b7fc95a8fd676c0
이준희 <wns8432@gmail.com>
2016-03-25 18:12:40 +0900

    GPGS, 애드몹 이그노어되어있었음.

---
+++ Unithon/Assets/Script/StartManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using System.Collections;
+using GoogleMobileAds.Api;
+
+public class StartManager : MonoBehaviour
+{
+    //Ads
+    BannerView bannerView;
+    // Use this for initialization
+    void Awake()
+    {
+        bannerView = new BannerView("ca-app-pub-5325622833123971/2069584440", AdSize.SmartBanner, AdPosition.Bottom);
+        AdRequest.Builder builder = new AdRequest.Builder();
+        //AdRequest request = builder.AddTestDevice(AdRequest.TestDeviceSimulator).AddTestDevice(SystemInfo.deviceUniqueIdentifier).Build();
+        AdRequest request = builder.Build();// 실제 빌드
+
+        bannerView.LoadAd(request); //배너 광고 요청
+        bannerView.Show();  // 배너 광고 출력
+    }
+
+    void Start()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+        .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+        Social.localUser.Authenticate((bool success) =>
+        {
+            //Debug.Log(success);
+        });
+    }
+    public void OnClick_Rank()
+    {
+        PlayGamesPlatform.Instance.ShowLeaderboardUI("CgkIyOj9mbIBEAIQAA");
+    }
+
+    public void OnClick_Start() 
+    { 
+    }
+
+    public void OnClick_Info()
+    {
+
+    }
+}
